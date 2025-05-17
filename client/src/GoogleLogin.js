8cdc0ad7060033b5279199e3699e535ca7f4b8a1
Aaditya Raj <69696351+aaditraj@users.noreply.github.com>
2025-05-16 18:27:36 -0700

    the session persists

--- client/src/GoogleLogin.js
+++ client/src/GoogleLogin.js
@@ -8,12 +8,9 @@ export default (props) => {
 	const responseGoogle = async (authResult) => {
 		try {
 			if (authResult["code"]) {
-				console.log(authResult.code);
 				const result = await googleAuth(authResult.code);
 				props.setUser(result.data.data.user);
-				alert("successfuly logged in");
 			} else {
-				console.log(authResult);
 				throw new Error(authResult);
 			}
 		} catch (e) {
